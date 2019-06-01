b942c6d021081794d5c747d8ee5eb7bb734ffc07
duasu3 <50001033+duasu3@users.noreply.github.com>
2019-06-01 11:24:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "+";\\ كرافكس
+const prefix = "+"
 
 client.on("message", async message => {
     var command = message.content.split(" ")[0];
@@ -88,7 +88,7 @@ client.on("message", async message => {
 });
 
 client.on("message", message => {
-    var prefix = "+";\\كرافكس
+    var prefix = "+"
         if (message.author.id === client.user.id) return;
         if (message.guild) {
        let embed = new Discord.RichEmbed()
